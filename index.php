f7b1e58f45f1bc494396e1efa88bf7b88dec2373
patricksmin <patricksmin@gmail.com>
2020-12-22 13:06:06 -0500

    added index.php file

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+$dbhost = 'localhost';
+$dbuser = 'root';
+$dbpass = '';
+
+$conn = mysql_connect($dbhost, $dbuser, $dbpass);
+mysql_select_db($dbname, $conn);
+
+$result = mysql_query("select * from users", $conn);
+
+print $result
+
+ ?>
